b18ff46525bde53d31d29250ecb5881df2f4fb2b
rainumdo <rainumdo@qq.com>
2025-06-08 23:34:19 +0800

    translate

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+if vim.filetype == "rust" or "c" then
+  vim.opt.expandtab = true
+  vim.opt.tabstop = 4
+  vim.opt.shiftwidth = 4
+  vim.opt.softtabstop = 4
+end
+
+vim.opt.clipboard = "unnamedplus"
---
+++ nvim/lua/plugins/translate.lua
@@ -0,0 +1,9 @@
+return {
+  --
+  {
+    "ianva/vim-youdao-translater",
+    keys = {
+      { "<leader>t", ":<C-u>Ydc<CR>" },
+    },
+  },
+}
