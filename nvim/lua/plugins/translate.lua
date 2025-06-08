b55c5fd68177b739798b7b9d9f08fd9f80030c12
rainumdo <rainumdo@qq.com>
2025-06-08 23:42:04 +0800

    translate

--- nvim/lua/plugins/translate.lua
+++ nvim/lua/plugins/translate.lua
@@ -3,7 +3,7 @@ return {
   {
     "ianva/vim-youdao-translater",
     keys = {
-      { "<leader>t", ":<C-u>Ydc<CR>" },
+      { "<leader>t", ":<C-u>Ydc<CR>", desc = "translater with youdao" },
     },
   },
 }
