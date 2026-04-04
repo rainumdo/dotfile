13f9b4d11db6182557c2556a250b80f8f066ff75
rainumdo <rainumdo.qq.com>
2026-04-04 23:00:08 +0800

    espeak translate

--- nvim/lua/plugins/translate.lua
+++ nvim/lua/plugins/translate.lua
@@ -1,23 +1,28 @@
--- sudo aptitude install espeak-ng
--- espeak-ng
-local function speak()
+-- sudo aptitude install espeak
+local function my_translate()
+  -- translate
+  local ok, _ = pcall(vim.cmd, "TranslateW")
+  if not ok then
+    vim.notify("翻译插件未加载", vim.log.levels.WARN)
+  end
+  -- espeak
   local word = vim.fn.expand("<cword>")
   if word == "" then
     return
   end
-  local cmd = string.format('espeak-ng -v en+f3 -s 100 "%s" &', word:gsub('"', '\\"'))
+  local cmd = string.format('espeak -v female3 -p 99 -s 125 "%s " &', word:gsub('"', '\\"'))
   vim.fn.system(cmd)
 end
 
-vim.keymap.set("n", "<leader>ts", speak, { desc = "speak" })
+vim.keymap.set("n", "<leader>t", my_translate)
 
 return {
   {
     "voldikss/vim-translator",
     keys = {
       -- { "<leader>t", ":TranslateW<CR>", desc = "translate" },
-      { "<leader>t", ":TranslateW<CR>", desc = "translate" },
     },
+    event = "VeryLazy",
     config = function()
       vim.g.translator_default_engines = { "bing", "haici" }
     end,
