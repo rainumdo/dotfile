fa02771a3b709751ef4ed7b4c4453a18657d797c
rainumdo <rainumdo.qq.com>
2026-04-04 17:33:56 +0800

    espeak-ng

--- nvim/lua/plugins/translate.lua
+++ nvim/lua/plugins/translate.lua
@@ -1,8 +1,21 @@
+-- sudo aptitude install espeak-ng
+-- espeak-ng
+local function speak()
+  local word = vim.fn.expand("<cword>")
+  if word == "" then
+    return
+  end
+  local cmd = string.format('espeak-ng -v en+f3 -s 100 "%s" &', word:gsub('"', '\\"'))
+  vim.fn.system(cmd)
+end
+
+vim.keymap.set("n", "<leader>ts", speak, { desc = "speak" })
+
 return {
-  --
   {
     "voldikss/vim-translator",
     keys = {
+      -- { "<leader>t", ":TranslateW<CR>", desc = "translate" },
       { "<leader>t", ":TranslateW<CR>", desc = "translate" },
     },
     config = function()
